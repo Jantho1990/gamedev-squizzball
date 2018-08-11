d261b72b7c7f44945370a44d2e0fe07435e194ee
Joshua Anthony <jantho1990@gmail.com>
2018-08-11 16:26:04 -0500

    added TileMap

--- pop/index.js
+++ pop/index.js
@@ -1,10 +1,11 @@
-import CanvasRenderer from "./renderer/CanvasRenderer"
-import Container from "./Container"
-import KeyControls from "./controls/KeyControls"
-import MouseControls from "./controls/MouseControls"
-import Sprite from "./Sprite"
-import Text from "./Text"
-import Texture from "./Texture"
+import CanvasRenderer from './renderer/CanvasRenderer'
+import Container from './Container'
+import KeyControls from './controls/KeyControls'
+import MouseControls from './controls/MouseControls'
+import Sprite from './Sprite'
+import Text from './Text'
+import Texture from './Texture'
+import TileMap from './TileMap'
 import TileSprite from './TileSprite'
 import Game from './Game'
 import math from './utils/math'
@@ -19,5 +20,6 @@ export default {
     Sprite,
     Text,
     Texture,
+    TileMap,
     TileSprite
 }
